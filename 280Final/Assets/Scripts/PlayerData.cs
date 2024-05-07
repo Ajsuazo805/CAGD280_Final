0da2bca011a4039d39ca93e747e55c4341cd8111
Ajsuazo805 <Suazoforlife@gmail.com>
2024-05-06 21:12:15 -0700

    We added collision code for player
    Power up collisions

--- 280Final/Assets/Scripts/PlayerData.cs
+++ 280Final/Assets/Scripts/PlayerData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerData : MonoBehaviour
 {
@@ -17,6 +18,12 @@ public class PlayerData : MonoBehaviour
     public float jumpForce = 8f;
 
     public bool onGround;
+
+    private bool poweredUp = false;
+
+    private float powerUpStart = -10f;
+    private int typeOfPowerUP = 0;
+    public float invincibilityDuration;
 
 
     // Start is called before the first frame update
@@ -58,5 +65,73 @@ public class PlayerData : MonoBehaviour
         
         Vector2 moveVector = playerInputActions.InGame.Move.ReadValue<Vector2>();
         playerRigidbody.transform.Translate(new Vector3(moveVector.x, 0) * speed);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+        print("GameObject tag " + other.tag);
+        if (other.tag == ("FirePower"))
+        {
+            Destroy(other.gameObject);
+            poweredUp = true;
+            powerUpStart = Time.time;
+            typeOfPowerUP = 1;
+        }
+        if (other.tag == ("StarPower"))
+        {
+            Destroy(other.gameObject);
+            poweredUp = true;
+            powerUpStart = Time.time;
+            typeOfPowerUP = 2;
+            StartCoroutine(Blink());
+        }
+        if (other.tag == ("IcePower"))
+        {
+            Destroy(other.gameObject);
+            poweredUp = true;
+            powerUpStart = Time.time;
+            typeOfPowerUP = 3;
+        }
+        if (other.tag == ("Enemy"))
+        {
+            if (typeOfPowerUP==1 || typeOfPowerUP==3)
+            {
+                poweredUp = false;
+                typeOfPowerUP = 0;
+            }
+            else
+            {
+                if (poweredUp && (powerUpStart + invincibilityDuration < Time.time) && typeOfPowerUP == 2)
+                {
+                    poweredUp = false;
+                    typeOfPowerUP = 0;
+                }
+                if (poweredUp == false)
+                {
+                    Destroy(this.gameObject);
+                    SceneManager.LoadScene(1);
+
+                }
+            }
+        }
+    }
+
+    public IEnumerator Blink()
+    {
+        for (int index = 0; index < 30; index++)
+        {
+            if (index % 2 == 0)
+            {
+                GetComponent<MeshRenderer>().enabled = false;
+            }
+            else
+            {
+                GetComponent<MeshRenderer>().enabled = true;
+            }
+            yield return new WaitForSeconds(.1f);
+        }
+        GetComponent<MeshRenderer>().enabled = true;
     }
 }
